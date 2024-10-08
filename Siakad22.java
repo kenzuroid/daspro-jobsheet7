a91aa5c92656ea4c5edd2e4f67ba16f1f6398051
kenzuroid <sholihahsitinikmatus@gmail.com>
2024-10-08 16:06:15 +0700

    percobaan 1

---
+++ Siakad22.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Siakad22 {
+
+    public static void main(String[] args) {
+        Scanner input22 = new Scanner(System.in);
+
+        double nilai, terendah = 100, tertinggi = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            nilai = input22.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
+    }
+}
